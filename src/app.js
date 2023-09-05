740ee33d0c6ae8833aec263539196a414e7db6a0
tihraqua <tihraquamahdi@gmail.com>
2023-09-05 18:04:56 +0200

    testing two commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wdnesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wndesday', 'Thursday', 'Friday', 'Saturday'];
 
     return days[date.getDay()];
 }
